d4a28d19b18445af8371f7f1e54eebed50aac596
jinseok <jinseock95@gmail.com>
2021-01-04 19:23:40 +0900

    W02_wordcount

--- W02/wordcount/views.py
+++ W02/wordcount/views.py
@@ -7,3 +7,12 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def result(request):
+    text = request.GET['fulltext'] # textarea에서 input하는 데이터
+    words = text.split()
+    word_dic = {}
+    for word in words:
+        word_dic[word] = word_dic.get(word, 0) + 1
+
+    return render(request,'result.html', {'full' : text, 'length' : len(words), 'dictionary' : word_dic.items()})
